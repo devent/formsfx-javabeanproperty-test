c58ec37e9ca997ba43f4615cc88e3d8412e9e276
Erwin Müller <erwin.mueller@deventm.org>
2021-11-03 09:32:13 +0100

    Add test that java bean property is not updated

---
+++ src/main/java/com/anrisoftware/formsfx_javabeanproperty_test/Demo.java
@@ -0,0 +1,57 @@
+package com.anrisoftware.formsfx_javabeanproperty_test;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+
+import com.anrisoftware.formsfx_javabeanproperty_test.ObservableGameMainPaneProperties.GameMainPaneProperties;
+import com.dlsc.formsfx.model.structure.Field;
+import com.dlsc.formsfx.model.structure.Form;
+import com.dlsc.formsfx.model.structure.Group;
+import com.dlsc.formsfx.view.renderer.FormRenderer;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
+
+public class Demo extends Application {
+
+    private Form form;
+
+    private GameMainPaneProperties p;
+
+    private ObservableGameMainPaneProperties np;
+
+    private BorderPane rootPanel;
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        p = new ObservableGameMainPaneProperties.GameMainPaneProperties();
+        np = new ObservableGameMainPaneProperties(p);
+        this.form = Form.of(Group.of(//
+                Field.ofStringType(np.kernelName).label("Name").required("Not empty"), //
+                Field.ofIntegerType(np.seed).label("Seed").required("Not empty"), //
+                Field.ofIntegerType(np.width).label("Width").required("Not empty"), //
+                Field.ofIntegerType(np.height).label("Height").required("Not empty"), //
+                Field.ofDoubleType(np.z).label("Z").required("Not empty"), //
+                Field.ofIntegerType(np.dim).label("Dimension").required("Not empty") //
+        ));
+        rootPanel = new BorderPane();
+        rootPanel.setCenter(new FormRenderer(form));
+        Scene scene = new Scene(rootPanel);
+        primaryStage.setTitle("Test");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+    @Override
+    public void stop() throws Exception {
+        System.out.println(p.toString());
+        assertThat(np.dim.get(), equalTo(4));
+        assertThat(p.dim, equalTo(4));
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
---
+++ src/main/java/com/anrisoftware/formsfx_javabeanproperty_test/MyLauncher.java
@@ -0,0 +1,6 @@
+package com.anrisoftware.formsfx_javabeanproperty_test;
+public class MyLauncher {
+    public static void main(String[] args){
+        Demo.main(args);
+    }
+}
